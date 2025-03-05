3c266bede33d447ac927a7b41b673d3cb8f87f7d
YadneshUmredkar75 <yadneshumredkar75@gmail.com>
2025-03-05 22:49:39 +0530

    delete

--- Food del/backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("Database is connected");
-  })
-}
-export default connectDB; 
